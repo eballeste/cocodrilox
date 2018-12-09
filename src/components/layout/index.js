16b91f760871f9eb8495abc13b5b6834c4e94d83
Enrique Balleste <eballeste@divisionof.com>
2018-12-09 17:27:43 -0500

    restructures site
    - also replaces layout css with sass module

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -3,8 +3,7 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
-import Header from './header'
-import './layout.css'
+import './layout.module.scss'
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -22,23 +21,13 @@ const Layout = ({ children }) => (
         <Helmet
           title={data.site.siteMetadata.title}
           meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
+            { name: 'description', content: 'portfolio site for artist Camila Buxeda' },
+            { name: 'keywords', content: 'camila buxeda puerto rico art cocodrilox' },
           ]}
         >
           <html lang="en" />
         </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div>
+        {children}
       </>
     )}
   />
