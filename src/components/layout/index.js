a3d63eff427bae616b62511fdc49f39f988faece
Enrique Balleste <eballeste@divisionof.com>
2018-12-10 00:38:41 -0500

    :art: updates styles, adds experimenting with css transitions

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
-import './layout.module.scss'
+import './layout.scss'
 
 const Layout = ({ children }) => (
   <StaticQuery
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,14 @@ import React from 'react'
 import Layout from 'components/layout'
 import Image from 'components/image'
 
+import styles from './index.module.scss'
+
 const IndexPage = () => (
   <Layout>
-    <div>
-      <Image />
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        <a href="https://instagram.com/cocodrilox"><Image /></a>
+      </div>
     </div>
   </Layout>
 )
