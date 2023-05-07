fc2d55f3461793c8f12ce586b883a266197845dd
eballeste <enrique.balleste@gmail.com>
2023-05-07 17:38:49 -0400

    clears out sourcemap warnings during dev

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,16 @@
 let mix = require('laravel-mix');
 
 mix
+  .js('./scripts/components/bird.js', 'assets')
   .js('./scripts/pages/home.js', 'assets')
   .sass('./styles/coco.scss', 'assets')
   .options({
     processCssUrls: false,
   });
+
+
+if (!mix.inProduction()) {
+  mix.webpackConfig({
+    devtool: 'inline-source-map'
+  })
+}
